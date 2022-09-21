4ff55c870efcf661894fc28b47e2abe6c1a3659c
Jun981020 <flwnsgud@naver.com>
2022-09-21 22:11:42 +0900

    Initial commit

---
+++ src/test/java/com/my/j_spring/JSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.j_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
